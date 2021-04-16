f7fb66801cb64bf3c0b1249b01ed816892dfe70f
Oscar Almqvist <almqvist.oscar@gmail.com>
2021-04-16 17:19:06 +0200

    Adds cool, new feature.

--- calculator.js
+++ calculator.js
@@ -2,3 +2,5 @@ exports.sum = (a, b) => a + b;
 
 exports.diff = (a, b) => a - b;
 
+exports.div = (a, b) => a / b;
+
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,4 +1,4 @@
-const { sum, diff } = require("../calculator")
+const { sum, diff, div } = require("../calculator")
 
 test("meaning of life", () => {
   expect(sum(32, 10)).toBe(42);
@@ -7,4 +7,8 @@ test("meaning of life", () => {
 
 test("difference", () => {
   expect(diff(123, 23)).toBe(100);
+})
+
+test("division", () => {
+  expect(div(4, 2)).toBe(2);
 })
